85d36b99db10d3a6ef6a8281ce77589bda12bd3b
moeinEN <moenayatti@gmail.com>
2024-06-04 16:37:27 +0330

    server main

---
+++ LinkedInServer/LinkedInServer/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import Controller.JwtHandler;
+import Database.DatabaseQueryController;
+import Database.DbController;
+import Http.HttpHandler;
+import Model.User;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.*;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //DatabaseQueryController.createTableUsers();
+
+        // Create HTTP server listening on port 8080
+        HttpServer httpServer = HttpHandler.makeConnectionPoint("localhost", 8080);
+        HttpHandler.createContext(httpServer);
+    }
+}
