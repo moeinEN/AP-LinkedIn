a6c8715fc0704bbd8f3fa39aa17f396fdb70809e
moeinEN <moenayatti@gmail.com>
2024-07-03 14:53:34 +0330

    complete NetworkRequest interface for login and Sign in

--- LinkedInClient/LinkedInClient/src/main/java/Controller/NetworkRequest.java
+++ LinkedInClient/LinkedInClient/src/main/java/Controller/NetworkRequest.java
@@ -1,8 +1,13 @@
 package Controller;
 
+import Model.Messages;
+import Model.Requests.LoginCredentials;
+import Model.Requests.RegisterCredentials;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
 public interface NetworkRequest {
     public JsonObject syncCallSayHello();
+    public Messages syncCallSignUp(RegisterCredentials registerCredentials);
+    public Messages syncCallLogin(LoginCredentials loginCredentials);
 }
